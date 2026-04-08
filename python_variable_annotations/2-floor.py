d91261e6d9d1b64845a580ffa65f757071b0e6e2
Ramos Hugo <132284948+hugou74130@users.noreply.github.com>
2026-04-08 14:10:08 +0200

    Add docstring to floor function
    Added a docstring to the floor function.

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python3
+"""Module for floor function with type annotations."""
+
+import math
+
+
 def floor(n: float) -> int:
-  return math.floor(n)
+    """Return the floor of a float as an int."""
+    return math.floor(n)
