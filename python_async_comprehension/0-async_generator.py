a2bbf36578df7099a84297b115031a1ae387f8e9
Ramos Hugo <132284948+hugou74130@users.noreply.github.com>
2026-04-10 14:02:09 +0200

    Implement async generator that yields random floats

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+from typing import Generator, AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
