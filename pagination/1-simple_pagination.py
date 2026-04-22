4104faa69866928f9db21a8ce10b0d82a2e0ce53
Ramos Hugo <132284948+hugou74130@users.noreply.github.com>
2026-04-22 11:50:43 +0000

    Add Server class for simple pagination of baby names
    Implement a Server class for paginating a dataset of popular baby names from a CSV file.

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+ import csv               
+  import math
+  from typing import List                                               
+                                                                        
+                                                                        
+  class Server:                                                         
+      """Server class to paginate a database of popular baby names.     
+      """                                                               
+      DATA_FILE = "Popular_Baby_Names.csv"                              
+                                                                        
+      def __init__(self):                                               
+          self.__dataset = None                                         
+                                                                        
+      def dataset(self) -> List[List]:                                  
+          """Cached dataset                                             
+          """                                                           
+          if self.__dataset is None:                                    
+              with open(self.DATA_FILE) as f:                           
+                  reader = csv.reader(f)                                
+                  dataset = [row for row in reader]                     
+              self.__dataset = dataset[1:]                              
+                                                                        
+          return self.__dataset                                         
+                                                                        
+      def get_page(self, page: int = 1, page_size: int = 10) ->         
+  List[List]:
+          """Get a page from the dataset."""                            
+          assert isinstance(page, int) and page > 0                     
+          assert isinstance(page_size, int) and page_size > 0           
+                                                                        
+          dataset = self.dataset()                                      
+          total = len(dataset)                                          
+          max_pages = (total + page_size - 1) // page_size              
+                                                                        
+          if page > max_pages:                                          
+              return []                                                 
+                                                                        
+          index_range = ((page - 1) * page_size, page * page_size)      
+          return dataset[index_range[0]:index_range[1]]
