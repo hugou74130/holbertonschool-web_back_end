b22eb582f8602c74eeb56f1d13d167be43ad0ff1
hugou74 <hugoramos1466@gmail.com>
2026-05-08 08:06:27 +0000

    fix: correct function parameters and syntax in updateStudentGradeByCity

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,11 @@
-import getStudentIdsSum from "./3-get_ids_sum";
-
-export default function updateStudentGradeByCity(student, city, newGrades){
-    return students 
-    .filter ((student) => student,location === city)
-    .map ((student) => {
-        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-        return {
-            ...student,
-            grade: gradeObj ? gradeObj.grade : 'N/A',
-        };
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
     });
 }
