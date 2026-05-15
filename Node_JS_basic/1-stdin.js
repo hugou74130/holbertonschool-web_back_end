80db4918b74d177dbee59cea3da3d25ce370d753
Ramos Hugo <hugoramos1466@gmail.com>
2026-05-15 18:42:17 +0200

    fix: correct formatting and indentation in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,15 +2,20 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (line) => {
-    console.log(`Your name is: ${line}`);
+  console.log(`Your name is: ${line}`);
+  if (process.stdin.isTTY) {
+    rl.close();
+  }
 });
 
-rl .on('close', () => {
+rl.on('close', () => {
+  if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
-});   
+  }
+});
