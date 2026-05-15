766b03ea3c3f847c1bc1f54fb3bbb9c48fb542f0
hugou74 <hugoramos1466@gmail.com>
2026-05-15 19:45:31 +0000

    Update 6-http_express.js: add explicit text/plain Content-Type

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245);
