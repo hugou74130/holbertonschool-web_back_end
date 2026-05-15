aef7a24167411b2de778573b2a4110e9a273a41d
Ramos Hugo <hugoramos1466@gmail.com>
2026-05-15 18:44:47 +0200

    refactor: simplify stdin handling by removing readline interface

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,9 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (line) => {
-  console.log(`Your name is: ${line}`);
-  if (process.stdin.isTTY) {
-    rl.close();
-  }
-});
-
-rl.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
